d87f302b1cf36a30d43734a4453a685020bc23e1
Prasanth kadambala <prasanth1108@gmail.com>
2021-01-05 16:03:59 +0530

    Add project files.

---
+++ ConsoleApp_StringReverse/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_StringReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter String");
+            string name = Console.ReadLine();
+            string reverse = "";
+            for (int i=name.Length-1; i>=0; i--)
+            {
+                reverse = reverse + name[i];
+            }
+            Console.WriteLine(reverse);
+            Console.Read();
+
+        }
+    }
+}
